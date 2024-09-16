f8e86978603147213fb5c09e6508586092843a1e
rbeninca@gmail.com <rbeninca@gmail.com>
2024-09-16 19:17:05 -0300

    i18n

--- app/src/main/java/com/example/contador/MainActivity.java
+++ app/src/main/java/com/example/contador/MainActivity.java
@@ -9,25 +9,10 @@ import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
-    Button button;
-    TextView tv;
-    int cont;//var button vazia (null)
-    @Override
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button =findViewById(R.id.bntclick);
-        tv =findViewById(R.id.textView);
-        button.setText("click");
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                cont++;
-                Button b=(Button) view;
-                b.setText(String.valueOf(cont));
-                tv.setText(String.valueOf(cont));
 
-            }
-        });
     }
 }
