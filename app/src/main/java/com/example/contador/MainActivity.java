8fde2a378b943a622b16b5fadb73941e53c2b078
Romulo Beninca <rbeninca@gmail.com>
2024-09-23 17:46:29 -0300

    exemplo saveStateOff

--- app/src/main/java/com/example/contador/MainActivity.java
+++ app/src/main/java/com/example/contador/MainActivity.java
@@ -1,47 +1,57 @@
 package com.example.contador;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
-    Button btnA,btnB;
+    SeekBar seekBar;
+    ProgressBar progressBar;
+    EditText edText;
+    TextView textView;
+
         @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-            btnA=findViewById(R.id.bntclickA);
-            btnB=findViewById(R.id.bntclickB);
-            btnA.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent= new Intent(getApplicationContext(),MainActivity2.class);
-                    startActivity(intent);
-                }
-            });
-
-            btnB.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent= new Intent(getApplicationContext(),MainActivity3.class);
-                    EditText editText;
-                    editText=findViewById(R.id.edText);
-                    String s=editText.getText().toString();
-                    intent.putExtra("msg",s);
-                    startActivity(intent);
-                }
-            });
-
-
+        edText = findViewById(R.id.edText);
+        textView = findViewById(R.id.textView);
+        seekBar = findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+            progressBar.setProgress(i);
+            textView.setText("Progresso: " + i + " / " + seekBar.getMax());
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+        progressBar = findViewById(R.id.progressBar);
         Log.d("Ciclo de vida", "passou pelo onCreate");
     }
 
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+    }
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
 
+    }
 }
