b9ed89a099f5f53b24342764fd51ff4e3436f2d6
Your Name <you@example.com>
2024-10-07 09:34:13 -0300

    lista i

--- listagem/src/main/java/com/example/listagem/MainActivity.java
+++ listagem/src/main/java/com/example/listagem/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    String[] nomes = new String[] {"Picareta", "Jhones Dark", "Camili","Danila","Thoinho"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
