6c8ecf011d2bedc0394043228ac91d62e603ab30
Your Name <you@example.com>
2024-10-07 19:36:54 -0300

    xml simplepaint add0

---
+++ paint/src/main/java/com/example/paint/SimplePaint.java
@@ -0,0 +1,14 @@
+package com.example.paint;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class SimplePaint extends View {
+
+    public SimplePaint(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
